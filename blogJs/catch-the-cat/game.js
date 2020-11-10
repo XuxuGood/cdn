633faabfdf0f27946d9969f51604c4b4594e2f91
XiaoXuxuy <v_xuxu@baidu.com>
2020-11-10 14:35:59 +0800

    优化

--- blogJs/catch-the-cat/game.js
+++ blogJs/catch-the-cat/game.js
@@ -1,13 +1,14 @@
 !function() {
-  const is_mobi = navigator.userAgent.toLowerCase().match(/(ipod|ipad|iphone|android|coolpad|mmp|smartphone|midp|wap|xoom|symbian|j2me|blackberry|wince)/i) != null;
-  var radius = is_mobi ? 12 : 20;
-  window.game = new CatchTheCatGame({ 
-      w: 11, 
-      h: 11, 
-      r: radius, 
-      backgroundColor: 0xeeeeee, 
-      parent: "catch-the-cat", 
-      statusBarAlign: "center", 
-      credit: " "
-  });
+    const is_mobi = navigator.userAgent.toLowerCase().match(/(ipod|ipad|iphone|android|coolpad|mmp|smartphone|midp|wap|xoom|symbian|j2me|blackberry|wince)/i) != null;
+    var radius = is_mobi ? 12 : 20;
+    var grid = is_mobi ? 9 : 11;
+    window.game = new CatchTheCatGame({
+        w: grid,
+        h: grid,
+        r: radius,
+        backgroundColor: 0xeeeeee,
+        parent: "catch-the-cat",
+        statusBarAlign: "center",
+        credit: " "
+    });
 }();
